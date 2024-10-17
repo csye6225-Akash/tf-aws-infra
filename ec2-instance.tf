728031b03559517b1fbbbabcec5cb0241833f02d
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-10-17 03:37:40 -0400

    added ec2

---
+++ ec2-instance.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web_app_instances" {
+  ami             = "ami-0035747b85a09457c"   # Replace with your AMI ID
+  instance_type   = "t2.micro"
+  # security_groups = [aws_security_group.web_sg.name]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  subnet_id = aws_subnet.public_subnets[0].id
+  associate_public_ip_address = "true"
+
+
+  root_block_device {
+    volume_size           = 25
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "web-app-instance"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "public_subnet_ids" {
   value = aws_subnet.public_subnets[*].id
 }
 
+output "first_public_subnet" {
+  value = aws_subnet.public_subnets[0].id
+  description = "ID of the first public subnet"
+}
+
 output "private_subnet_ids" {
   value = aws_subnet.private_subnets[*].id
 }
@@ -20,4 +25,13 @@ output "private_route_table_id" {
   value = aws_route_table.private_route_table.id
 }
 
+output "security_group_id" {
+  description = "The ID of the security group"
+  value       = aws_security_group.web_sg.id
+}
+
+output "security_group_name" {
+  description = "The name of the security group"
+  value       = aws_security_group.web_sg.name
+}
 
