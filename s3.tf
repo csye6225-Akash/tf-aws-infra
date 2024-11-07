5e7c06c8fa0c3fc03027d39e9db0e18cbfa18f8b
AkashChhabria <113483019+AkashChhabria@users.noreply.github.com>
2024-11-06 21:56:01 -0500

    autoscaling

--- s3.tf
+++ s3.tf
@@ -36,9 +36,11 @@ resource "random_id" "bucket_id" {
 resource "aws_s3_bucket" "private_bucket" {
   bucket = "akash-bucket-${formatdate("YYYYMMDD", timestamp())}-${random_id.bucket_id.hex}"
 
-  # Ensure the bucket is private
-  acl           = "private" # This is optional; you can remove it if you're using bucket policies.
   force_destroy = true
+}
+resource "aws_s3_bucket_acl" "private_bucket_acl" {
+  bucket = aws_s3_bucket.private_bucket.id
+  acl    = "private"
 }
 
 # If you need to set a bucket policy, you can do it like this
@@ -138,16 +140,6 @@ resource "aws_iam_role_policy_attachment" "attach_extra_policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-# resource "aws_route53_record" "app" {
-#   zone_id = var.zone_id # Use your Zone ID variable directly
-#   name    = "${var.subdomain}.${var.domain_name}"
-#   type    = "A"
-#   ttl     = 300
-#   records = [aws_instance.web_app_instances.public_ip]
-
-#   depends_on = [aws_instance.web_app_instances]
-# }
-
 resource "aws_route53_record" "csye6225_dns_record" {
   zone_id = var.zone_id
   name    = "${var.subdomain}.${var.domain_name}"
@@ -182,7 +174,7 @@ data "aws_iam_policy_document" "ec2_assume_role_policy" {
   }
 }
 resource "aws_route53_zone" "dev_zone" {
-  name = "dev.akashchhabria.me"
+  name = "${var.subdomain}.${var.domain_name}"
 }
 
 
